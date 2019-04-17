03a3d1c41500ff7bdf865a647fe66e20b2534370
zhangsen <zhangsen@cnhqd.net>
2019-04-17 09:21:37 +0800

    test case

--- AppTest/testCase/Sprint1/case_Sprint1_android_0004.py
+++ AppTest/testCase/Sprint1/case_Sprint1_android_0004.py
@@ -16,25 +16,23 @@ class MyTestCase(unittest.TestCase):
     @classmethod
     def tearDown(self):
         BaseOperate.report_screen_shot(self, self.case_name)
-        BaseOperate.uninstallApp(self, PhoneControl.package_name)
+        # BaseOperate.uninstallApp(self, PhoneControl.package_name)
         BaseOperate.quit(self)
 
     def test_step(self):
-        u"""我的资料必填项姓名修改为空"""
+        u"""我的资料必填项邮箱修改为空"""
         BaseOperate.startActivity(self, PhoneControl.package_name, PhoneControl.activity_name)
 
         logger.info("APP登录")
         BaseOperate.app_login(self, Content.register_count, Content.login_password)
 
-        logger.info("判断真实姓名是否可以设置为空")
+        logger.info("判断邮箱是否可以设置为空")
         BaseOperate.touch_id_by_index(self, PhoneControl.me)
         BaseOperate.touch_id_by_index(self, PhoneControl.id_me_icon)
-        BaseOperate.touch_id_by_index(self, PhoneControl.id_true_name)
+        BaseOperate.touch_id_by_index(self, PhoneControl.id_mail)
         BaseOperate.clear_text_by_id(self, PhoneControl.id_infor_modify)
         BaseOperate.touch_id_by_index(self, PhoneControl.id_toolbar_right_tv, t=0)
-        statue = BaseOperate.find_toast(self, "真实姓名不能为空")
+        statue = BaseOperate.find_toast(self, "请输入正确的邮箱地址")
         self.assertTrue(statue)
 
 
-
-    
